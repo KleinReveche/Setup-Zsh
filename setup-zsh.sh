088ad185bd412875ff13ee95621678f9a2ecb8eb
Klein Reveche <therealklr2003@gmail.com>
2020-11-14 18:33:26 +0800

    add: modified oh-my-zsh installer

--- setup-zsh.sh
+++ setup-zsh.sh
@@ -2,7 +2,8 @@
 
 sudo apt update & sudo apt upgrade -y
 sudo apt install zsh wget -y
-sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(wget -O- https://raw.githubusercontent.com/KleinReveche/Setup-Zsh/main/oh-my-zsh-installer.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+rm .zshrc
 wget https://raw.githubusercontent.com/KleinReveche/Setup-Zsh/main/.zshrc
 wget https://raw.githubusercontent.com/KleinReveche/Setup-Zsh/main/.p10k.zsh
