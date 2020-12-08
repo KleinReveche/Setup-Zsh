3a10f3051c22a34efa6292b720cfcfb21a19396e
Klein Reveche <therealklr2003@gmail.com>
2020-12-09 07:24:22 +0800

    fix: wrong p10k config extension

--- setup-zsh.sh
+++ setup-zsh.sh
@@ -4,7 +4,7 @@ sh -c "$(wget -O- https://raw.githubusercontent.com/KleinReveche/Setup-Zsh/main/
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k > /dev/null
 rm ~/.zshrc
 
-wget -O ~/.p10k.sh https://raw.githubusercontent.com/KleinReveche/Setup-Zsh/main/.p10k.zsh -o /dev/null
+wget -O ~/.p10k.zsh https://raw.githubusercontent.com/KleinReveche/Setup-Zsh/main/.p10k.zsh -o /dev/null
 if (( $EUID != 0 )); then
 wget -O ~/.zshrc https://raw.githubusercontent.com/KleinReveche/Setup-Zsh/main/.zshrc -o /dev/null
     exit
