71e5527f874bd99c17608bc62b1d84b7bc895f5e
Klein Reveche <therealklr2003@gmail.com>
2020-12-09 04:27:17 +0800

    change: minor fixes

--- setup-zsh.sh
+++ setup-zsh.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-sh -c "$(wget -O- https://raw.githubusercontent.com/KleinReveche/Setup-Zsh/main/oh-my-zsh-installer.sh)"
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+sh -c "$(wget -O- https://raw.githubusercontent.com/KleinReveche/Setup-Zsh/main/oh-my-zsh-installer.sh)" > /dev/null
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k > /dev/null
 rm ~/.zshrc
 
-wget -O ~/.p10k.sh https://raw.githubusercontent.com/KleinReveche/Setup-Zsh/main/.p10k.zsh
+wget -O ~/.p10k.sh https://raw.githubusercontent.com/KleinReveche/Setup-Zsh/main/.p10k.zsh -o /dev/null
 if (( $EUID != 0 )); then
-wget -O ~/.zshrc https://raw.githubusercontent.com/KleinReveche/Setup-Zsh/main/.zshrc
+wget -O ~/.zshrc https://raw.githubusercontent.com/KleinReveche/Setup-Zsh/main/.zshrc -o /dev/null
     exit
 fi
-wget -O ~/.zshrc https://raw.githubusercontent.com/KleinReveche/Setup-Zsh/main/root.zshrc
+wget -O ~/.zshrc https://raw.githubusercontent.com/KleinReveche/Setup-Zsh/main/root.zshrc -o /dev/null
+source ~/p10k.zsh
+echo 'ZSH Setup Done!'
