40e0d1141526c6dd883a52f290a8ca6b86535aae
Klein Reveche <therealklr2003@gmail.com>
2020-12-08 10:37:24 +0800

    tweaks: minor changes to setup-script

--- setup-zsh.sh
+++ setup-zsh.sh
@@ -2,11 +2,11 @@
 
 sh -c "$(wget -O- https://raw.githubusercontent.com/KleinReveche/Setup-Zsh/main/oh-my-zsh-installer.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-rm .zshrc
+rm ~/.zshrc
 
-wget https://raw.githubusercontent.com/KleinReveche/Setup-Zsh/main/.p10k.zsh
+wget -O ~/.p10k.sh https://raw.githubusercontent.com/KleinReveche/Setup-Zsh/main/.p10k.zsh
 if (( $EUID != 0 )); then
-wget https://raw.githubusercontent.com/KleinReveche/Setup-Zsh/main/.zshrc
+wget -O ~/.zshrc https://raw.githubusercontent.com/KleinReveche/Setup-Zsh/main/.zshrc
     exit
 fi
-wget -O .zshrc https://raw.githubusercontent.com/KleinReveche/Setup-Zsh/main/root.zshrc
+wget -O ~/.zshrc https://raw.githubusercontent.com/KleinReveche/Setup-Zsh/main/root.zshrc
