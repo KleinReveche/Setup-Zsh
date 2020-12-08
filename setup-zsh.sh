1229161b4ac4f971a9491d13c1ad2245f604e5af
Klein Reveche <therealklr2003@gmail.com>
2020-12-08 08:05:24 +0800

    add: check for root for proper zshrc

--- setup-zsh.sh
+++ setup-zsh.sh
@@ -5,5 +5,10 @@ sudo apt install zsh wget -y
 sh -c "$(wget -O- https://raw.githubusercontent.com/KleinReveche/Setup-Zsh/main/oh-my-zsh-installer.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 rm .zshrc
-wget https://raw.githubusercontent.com/KleinReveche/Setup-Zsh/main/.zshrc
+
 wget https://raw.githubusercontent.com/KleinReveche/Setup-Zsh/main/.p10k.zsh
+if (( $EUID != 0 )); then
+wget https://raw.githubusercontent.com/KleinReveche/Setup-Zsh/main/.zshrc
+    exit
+fi
+wget -O .zshrc https://raw.githubusercontent.com/KleinReveche/Setup-Zsh/main/root.zshrc
